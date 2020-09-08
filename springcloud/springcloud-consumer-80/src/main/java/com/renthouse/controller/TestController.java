7a86a48cd3481af65b1c11eb091b345b78e4c6c5
LiangJingXing <17714575715@163.com>
2020-09-08 15:16:10 +0800

    provider code

--- springcloud/springcloud-consumer-80/src/main/java/com/renthouse/controller/TestController.java
+++ springcloud/springcloud-consumer-80/src/main/java/com/renthouse/controller/TestController.java
@@ -2,17 +2,21 @@ package com.renthouse.controller;
 
 import com.renthouse.pojo.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
-@RestController("consumer")
+@RestController
 public class TestController {
 
     /** 前缀 **/
-    private static final String REST_PREFIX = "http://loaclhost:8001";
+    private static final String REST_PREFIX = "http://localhost:8001";
 
     // 提供远程访问HTTP的方法
     @Autowired
@@ -22,8 +26,18 @@ public class TestController {
      * 查询所有测试信息
      * @return
      */
-    @GetMapping("test/list")
+    @RequestMapping("/consumer/list")
     public List<Test> queryAll(){
         return restTemplate.getForObject(REST_PREFIX+"/test/list",List.class);
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping("/consumer/get{id}")
+    public Test getTestById(@PathVariable("id")Long id){
+        return restTemplate.getForObject(REST_PREFIX+"/test/get/"+id,Test.class);
     }
 }
